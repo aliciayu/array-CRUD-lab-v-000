75d79cbe5345e611d14b2422f5fb6f3d653b49c7
Alicia Yu <alicia.yu.16@gmail.com>
2017-09-29 22:52:17 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["one", "two", "three", "four"]
   element = "Array"
-  add_element_to_end_of_array(array, element) << "five"
+  add_element_to_end_of_array << "five"
 end
 
 def add_element_to_start_of_array(array, element)
